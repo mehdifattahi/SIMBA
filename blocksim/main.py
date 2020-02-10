4082d288298549c424e919224dfe30a683490afb
fattahi_mehdi <fattahi.mehdi@gmail.com>
2020-02-09 17:37:26 -0800

    change simulation duration.

--- blocksim/main.py
+++ blocksim/main.py
@@ -36,7 +36,7 @@ def report_node_chain(world, nodes_list):
 
 def run_model():
     now = int(time.time())  # Current time
-    duration = 36000  # seconds
+    duration = 360000  # seconds
 
     world = SimulationWorld(
         duration,
