9baa807233f988c7ecf4bf2e8836432dc67e4d55
fattahi_mehdi <fattahi.mehdi@gmail.com>
2020-05-19 15:40:15 -0700

    add BlockPropagation methods.

--- blocksim/DBConnection.py
+++ blocksim/DBConnection.py
@@ -12,4 +12,23 @@ class DBConnection:
 
     def getAllBlock_verification(self):
         self.mu.initialize_collection("block_verification")
-        return self.mu.find_all()
+        return self.mu.find_all_withoutID()
+
+    def inserBlock_propagation(self, connection, blocks):
+        self.mu.initialize_collection("block_propagation")
+        self.mu.insert({'connection': connection, 'blocks': blocks})
+        # print(self.mu.find_all())
+
+    def updateBlock_propagationList(self, connection, blocks):
+        self.mu.initialize_collection("block_propagation")
+        self.mu.update(connection, blocks)
+
+    def updateBlock_propagationDict(self, connection, blocks):
+        self.mu.initialize_collection("block_propagation")
+        self.mu.updateDict(connection, blocks)
+
+        # {'$addToSet': {'blocks': {'$each': [{'PPYAY357ae9': 0.23460006713867188}, {'OOYAY357ae9': 0.23460006713867188}]}}})
+
+    def getBlockPropagation(self, connection):
+        self.mu.initialize_collection("block_propagation")
+        return self.mu.getBlockPropagation(connection)
